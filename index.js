7dfbe181f8a5d05ae71ffe1bd42b67d8ecf9316d
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-04-24 20:54:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicianArray, instrumentArray) {
   let resultArray = []
-  for (i = 0; i < musicianArray.length(), i++) {
+  for (i = 0; i < musicianArray.length(); i++) {
     resultString = `${musicianArray[i]} plays ${instrumentArray[i]}`
     resultArray.push(resultString)
   }
